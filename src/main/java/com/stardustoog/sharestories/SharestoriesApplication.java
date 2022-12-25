26f0bda6c9236ea799f50a0a95c935eb3a531ed0
Nerdy01 <serversforfree7@gmail.com>
2022-12-25 11:35:45 +0400

    commit to upload project

---
+++ src/main/java/com/stardustoog/sharestories/SharestoriesApplication.java
@@ -0,0 +1,13 @@
+package com.stardustoog.sharestories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharestoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharestoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stardustoog/sharestories/SharestoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardustoog.sharestories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharestoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
